5adaf7223c2f97d90239ce0e6b9225b410b60ac8
Darpan3011 <darpankanani301@gmail.com>
2024-12-01 16:10:48 +0530

    Update code for Role Seeding

--- finalSubmissionDotNet/Program.cs
+++ finalSubmissionDotNet/Program.cs
@@ -1,6 +1,7 @@
 using Serilog;
 using finalSubmissionDotNet.BuilderExtensions;
 using finalSubmissionDotNet.Middleware;
+using finalSubmission.Infrastructure.ISeeder;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,19 +14,30 @@ builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider services, 
 
 builder.Services.AddServiceCollection(builder.Configuration);
 
+// Build the application
 WebApplication app = builder.Build();
 
+// Seed roles
+await SeedRolesAsync(app.Services);
+
+// Middleware setup
 app.UseExceptionMiddleware();
-
 app.UseSerilogRequestLogging();
-
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
+
+// Seed roles
+async Task SeedRolesAsync(IServiceProvider services)
+{
+    using (var scope = services.CreateScope())
+    {
+        var roleSeeder = scope.ServiceProvider.GetRequiredService<IRoleSeeder>();
+        await roleSeeder.SeedRolesAsync();
+    }
+}
