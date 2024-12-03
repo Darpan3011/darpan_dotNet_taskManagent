9ec6a715f57e8619ab18160b38cd94a168ac85a0
KananiDarpanAshokbha <KananiDarpanAshokbha@VLPO98>
2024-12-03 16:54:47 +0530

    addded the sps

---
+++ finalSubmission.Infrastructure/Migrations/20241203111106_sp_getalltasks.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace finalSubmission.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class sp_getalltasks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE PROCEDURE GetAllTasks
+                AS
+                BEGIN
+                    SELECT * FROM AllTasksTable;
+                END;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DROP PROCEDURE IF EXISTS GetAllTasks;
+            ");
+
+        }
+    }
+}
---
+++ finalSubmission.Infrastructure/Migrations/20241203111654_sp_getallusers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace finalSubmission.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class sp_getallusers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE PROCEDURE GetAllUsers
+                AS
+                BEGIN
+                    SELECT * FROM AllUsersTable;
+                END;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DROP PROCEDURE IF EXISTS GetAllUsers;
+            ");
+        }
+    }
+}
